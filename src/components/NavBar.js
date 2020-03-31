9d3f6f3325585e7efa79d99a5a31c19f6cb51f00
abenheni <a.benheni@gmail.com>
2020-03-31 22:42:34 +0200

    connect to authcontext

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class NavBar extends Component {
-    static contextType = ThemeContext;
 
     render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return(
-            <nav style={{ background: theme.ui, color: theme.content }}>
-                <h1>Moudir Context Hook</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        );
+        return (
+        <AuthContext.Consumer>{(authContext) => (
+                <ThemeContext.Consumer>{(themeContext) => {
+                    const { isAuthenticated, toggleAuth } = authContext;
+                    const { isLightTheme, light, dark } = themeContext;
+                    const theme = isLightTheme ? light : dark;        
+                    return(
+                        <nav style={{ background: theme.ui, color: theme.content }}>
+                            <h1>Moudir Context Hook</h1>
+                            <div className='auth-box' onClick={ toggleAuth }>
+                                { isAuthenticated ? 'Welcome User!' : 'Log in'}
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    );
+                }}
+                </ThemeContext.Consumer>
+            )}
+        </AuthContext.Consumer>)
     }
 }
 
